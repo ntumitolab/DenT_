0ab04385c938a0883a84b059216ef156546b7177
Chan-Min Hsu <32631423+VillaHsu@users.noreply.github.com>
2021-10-29 14:16:50 +0800

    Update split_data.py

--- data/split_data.py
+++ data/split_data.py
@@ -8,13 +8,25 @@ def split(train, test):
     parser.add_argument('--src', help='path to dataset', default='Mito')
     parser.add_argument('--train_size', default=0.75, help='training set size')
     args = parser.parse_args()
-
+    
+    if not os.path.exists(os.path.join(args.src, train)):
+        os.makedirs(os.path.join(args.src, train))
+    if not os.path.exists(os.path.join(args.src, test)):
+        os.makedirs(os.path.join(args.src, test))        
     source = os.path.join(args.src, train)
     dest = os.path.join(args.src, test)
 
+    if not os.path.exists(os.path.join(source, 'target')):
+        os.makedirs(os.path.join(source, 'target'))
+    if not os.path.exists(os.path.join(source, 'source')):
+        os.makedirs(os.path.join(source, 'source'))     
     source_Mito = os.path.join(source, 'target')
     source_TL = os.path.join(source, 'source')
 
+    if not os.path.exists(os.path.join(dest, 'target')):
+        os.makedirs(os.path.join(dest, 'target'))
+    if not os.path.exists(os.path.join(dest, 'source')):
+        os.makedirs(os.path.join(dest, 'source'))     
     dest_Mito = os.path.join(dest, 'target')
     dest_TL = os.path.join(dest, 'source')
 
