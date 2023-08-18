d3aa2d5a879e59aff9250cc05b682bdd0d842779
r10945018 <r10945018@ntu.edu.tw>
2023-08-19 03:46:48 +0800

    [bash] 2 related file

---
+++ ShangRu_202307_Test/bash/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# training parameters are mentioned in paper ( B. Implementation Details )
+#
+# --target_image "" \  # predict objects: target, target_dna
+
+
+python "../../test.py" \
+    --data_path "../../data/{DataSet}_DenT/" \
+    --target_image "target" \
+    --source_image "source" \
+    --gpu 0 \
+    --test_batch 1 \
+    --model "DenT" \
+    --checkpoints "../checkpoints/DenT_target_15725" \
+    --random_seed 123 \
+    --seg_dir "../seg_results/" \
+
+    # --patch \ # KiUnet parameters
