c5a9155ae775abe002429b77d2afae3d385dae94
twsqzqy988 <twsqzqy988@ln01-twnia2.twcc.ai>
2023-09-26 09:52:55 +0800

    [module: new_fns] 2 new functions

--- ShangRu_202307_Test/utils.py
+++ ShangRu_202307_Test/utils.py
@@ -1,6 +1,12 @@
 import subprocess # ShangRu_202307_Test
 
-def print_nvidia_smi(): # ShangRu_202307_Test
+import random
+import numpy as np
+import torch
+# -----------------------------------------------------------------------------/
+
+
+def print_nvidia_smi():
     """ show `nvidia-smi`
     """ 
     # 要執行的命令
@@ -13,3 +19,31 @@ def print_nvidia_smi(): # ShangRu_202307_Test
     output_str = output.decode('utf-8')
     # 輸出結果
     print(f"{output_str}")
+    # -------------------------------------------------------------------------/
+
+
+
+def set_reproducibility(seed):
+    """ Pytorch reproducibility
+        - ref: https://clay-atlas.com/us/blog/2021/08/24/pytorch-en-set-seed-reproduce/?amp=1
+        - ref: https://pytorch.org/docs/stable/notes/randomness.html
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed) # current GPU
+    torch.cuda.manual_seed_all(seed) # all GPUs
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    # -------------------------------------------------------------------------/
+
+
+
+def seed_worker(worker_id):
+    """ DataLoader reproducibility
+        ref: 'https://pytorch.org/docs/stable/notes/randomness.html'
+    """
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+    # -------------------------------------------------------------------------/
