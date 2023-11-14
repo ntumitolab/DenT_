49a024aecf0b2f54371a832a1d76ba8831339be2
twsqzqy988 <twsqzqy988@ln01-twnia2.twcc.ai>
2023-11-14 16:45:47 +0800

    [script: update] train.py

--- train.py
+++ train.py
@@ -17,52 +17,29 @@ from utils import BCEDiceLoss
 import warnings
 warnings.filterwarnings("ignore", '.*output shape of zoom.*')
 
-from ShangRu_202307_Test.utils import print_nvidia_smi, \
-    set_reproducibility, seed_worker # ShangRu_202307_Test
+# ShangRu_202307_Test
+import sys
+from pathlib import Path
+from datetime import datetime
+
+abs_module_path = Path("/work/twsqzqy988/DenT-PaperRevision").resolve()
+if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
+    sys.path.insert(0, str(abs_module_path)) # add path to scan customized module
+
+from misc_utils import print_nvidia_smi, \
+    set_reproducibility, seed_worker, get_args, set_args_dirs, dump_config
 # -----------------------------------------------------------------------------/
 
 def save_model(model, save_path):
     torch.save(model.state_dict(), save_path)
 
 def main():
-    parser = argparse.ArgumentParser(description='Mito-Net')
-    # Datasets parameters
-    parser.add_argument('--data_path', type=str, default='data/Z_stack', help="root path to data directory")
-    parser.add_argument('--workers', type=int, default=4, help="number of data loading workers")
-    parser.add_argument('--target_image', type=str, default='Mito', help='which target to be predicted')
-    parser.add_argument('--source_image', type=str, default='TL', help='which source to be trained')
-
-    # Training parameters
-    parser.add_argument('--gpu', default=0, type=int)
-    parser.add_argument('--epoch', default=200, type=int, help="number of training iterations")
-    parser.add_argument('--val_epoch', default=1, type=int, help="number of validation iterations")
-    parser.add_argument('--train_batch', default=2, type=int)
-    parser.add_argument('--lr', default=0.0002, type=float)
-    parser.add_argument('--weight_decay', default=0.0005, type=float)
-
-    parser.add_argument('--model', type=str, default='DenT')
-    parser.add_argument('--log_dir', type=str, default='logs')
-    parser.add_argument('--checkpoints', type=str, default='checkpoints')
-    parser.add_argument('--random_seed', type=int, default=123)
-    parser.add_argument('--seg_dir', type=str, default='seg_results') # ShangRu_202307_Test
-    parser.add_argument('--add_pos_emb', action=argparse.BooleanOptionalAction)
-    parser.add_argument('--use_multiheads', nargs='+', type=int, default=[1, 1, 1, 1])
-
-    # deep supervision
-    parser.add_argument('--deep_supervision', type=bool, default=False)
-    # Patches
-    parser.add_argument('--patch', type=bool, default=False)
-
-    args = parser.parse_args()
-
-    if not os.path.exists(args.log_dir):
-        os.makedirs(args.log_dir)
-    if not os.path.exists(args.checkpoints):
-        os.makedirs(args.checkpoints)
+    
+    args = get_args("train")  # ShangRu_202307_Test
 
     ''' Setup GPU '''
-    torch.cuda.set_device(args.gpu)
-    # torch.cuda.empty_cache() # ShangRu_202307_Test
+    # torch.cuda.set_device(args.gpu)
+    torch.cuda.empty_cache() # ShangRu_202307_Test
 
     ''' Setup Random Seed '''
     set_reproducibility(args.random_seed) # ShangRu_202307_Test
@@ -70,10 +47,11 @@ def main():
     g.manual_seed(0) # ShangRu_202307_Test
     seed = np.random.randint(100000)
 
-    if not os.path.exists(os.path.join(args.checkpoints, '{}_{}_{}'.format(args.model, args.target_image, seed))):
-        os.makedirs(os.path.join(args.checkpoints, '{}_{}_{}'.format(args.model, args.target_image, seed)))
-    if not os.path.exists(os.path.join(args.log_dir, 'Train_info_{}_{}_{}'.format(args.model, args.target_image, seed))):
-        os.makedirs(os.path.join(args.log_dir, 'Train_info_{}_{}_{}'.format(args.model, args.target_image, seed)))
+    ''' Set dirs '''
+    set_args_dirs(args, seed, "train") # ShangRu_202307_Test
+    time_stamp: str = datetime.now().strftime('%Y%m%d_%H_%M_%S')
+    print(f"datetime: {time_stamp}\n")
+    dump_config(Path(args.log_dir).joinpath(f"{time_stamp}_args.toml"), args.__dict__) # ShangRu_202307_Test
 
     ''' Load Dataset and Prepare Dataloader '''
     print('===> Preparing dataloader ... ')
@@ -116,13 +94,13 @@ def main():
                            weight_decay=args.weight_decay)
 
     ''' Setup Tensorboard '''
-    writer = SummaryWriter(os.path.join(args.log_dir, 'Train_info_{}_{}_{}'.format(args.model, args.target_image, seed)))
+    writer = SummaryWriter(args.log_dir) # ShangRu_202307_Test
     
     ''' Train Model '''
     print('===> Start training ...\n') # ShangRu_202307_Test
     iters = 0
     best_mIoU = 0
-    best_val_loss = 1
+    best_val_loss = np.inf # ShangRu_202307_Test
     for epoch in range(1, args.epoch + 1):
         model.train()
         for idx, (img_ids, imgs, segs) in enumerate(train_loader): # ShangRu_202307_Test
@@ -169,19 +147,22 @@ def main():
 
             # Save Best Model 
             if val_loss < best_val_loss - 1e-4:
-                save_model(model, os.path.join(args.checkpoints, '{}_{}_{}'.format(args.model, args.target_image, seed), 'model_{}_best_pth.tar'.format(args.model)))
+                save_model(model, Path(args.checkpoints).joinpath(f"model_{args.model}_best_pth.tar")) # ShangRu_202307_Test
                 #best_mIoU = mIoU
                 best_val_loss = val_loss
                 best_epoch = epoch
         
         ''' Save Model (Define in above)'''
         if epoch % 50 == 0:
-            save_model(model, os.path.join(args.checkpoints, '{}_{}_{}'.format(args.model, args.target_image, seed), 'model_{}_{}_pth.tar'.format(args.model, epoch)))
+            save_model(model, Path(args.checkpoints).joinpath(f"model_{args.model}_{epoch}_pth.tar")) # ShangRu_202307_Test
     
     print('The best model occurred in Epoch [{}] with validation loss {}'.format(best_epoch, best_val_loss))
-
-    print(); print_nvidia_smi() # ShangRu_202307_Test
+    print()
+    print_nvidia_smi() # ShangRu_202307_Test
     # writer.add_graph(model, imgs) # ShangRu_202307_Test ( 改成 evaluate 再存 )
+    # -------------------------------------------------------------------------/
+
+
 
 if __name__ == '__main__':
 	main()
