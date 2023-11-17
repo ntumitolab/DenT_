8ec2568a2f0921413fbec4f82debc60729025cf2
twsqzqy988 <twsqzqy988@ln01-twnia2.twcc.ai>
2023-11-18 07:39:22 +0800

    [upload] calcu_score.sh

---
+++ script/calcu_score.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+module purge
+module load miniconda3
+conda activate "DenT-tensorboard-py310"
+
+# variables
+target="target" # options: target / target_dna
+model="CusDenT" # options: (DenT) DenT, CusDenT(revision)
+data_root="../data"
+result_root="../results"
+record_dir="CusDenT_target_3D_15725_1" # change result dir
+
+python "../calcu_score.py" \
+    --seg_results "$result_root/$target/$record_dir/seg_results" \
+    --dent_seg_gt "$data_root/DenT/test/$target"
